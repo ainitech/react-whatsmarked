3db6546497dbcb459ac290ee2605a593861e109c
AINI TECH <72950126+ainitech@users.noreply.github.com>
2025-09-21 15:29:04 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,8 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        // transpila para ES5, compatível até com IE11
-        targets: {
-          browsers: [">0.25%", "not dead", "ie 11"]
-        },
-        useBuiltIns: "usage", // injeta polyfills só quando necessário
-        corejs: 3             // usa core-js v3
+        targets: { browsers: [">0.25%", "not dead"] }, // gera ES5 sem core-js
+        modules: "auto"
       }
     ],
     "@babel/preset-react"
